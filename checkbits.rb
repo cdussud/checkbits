42ac80c2e4c84796b072501d1f56c2b58ceb2044
Cedric Dussud <cedric@wework.com>
2017-04-18 21:59:13 -0400

    Write json file with checksums

--- checkbits.rb
+++ checkbits.rb
@@ -1,24 +1,26 @@
 require 'digest'
 require 'find'
-
-sha256 = Digest::SHA256.file('checkbits.rb')
-puts "My checksum is #{sha256.base64digest}"
+require 'oj'
 
 directory = ARGV.first
 puts "Base directory is #{directory}"
 
-
 def system_name?(path)
   File.basename(path)[0] == '.'
 end
+
+checksum_map = {}
 
 # Recursive directory walk using the Find module
 Find.find(directory) do |path|
   if File.directory?(path)
     Find.prune if system_name?(path)
   elsif !system_name?(path)
-    puts path
-    puts Digest::SHA256.file(path).base64digest
-    puts "\n"
+    checksum = Digest::SHA256.file(path).base64digest
+    checksum_map[checksum] = [path, Time.now]
   end
 end
+
+json = Oj.dump(checksum_map)
+File.open('checkbits.json', 'w') { |f| f.write(json) }
+puts "Successfully wrote json file"
