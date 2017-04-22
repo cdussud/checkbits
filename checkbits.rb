ce2ad2f7b48e4dcdc0904682220dd80c85b4dc3a
Cedric Dussud <cedric@wework.com>
2017-04-21 21:49:20 -0400

    Added timestamps to the log output

--- checkbits.rb
+++ checkbits.rb
@@ -3,10 +3,6 @@ require 'find'
 require 'bundler'
 Bundler.require
 
-# TODO
-# Next up
-#  - output elapsed time
-
 CHECKSUM_FILE = 'checkbits.json'
 MAX_FILE_SIZE = 30_000_000 # 30MB
 
@@ -45,16 +41,19 @@ rescue => e
 end
 
 new_checksums = {}
+failed_files = []
 changed_files = []
 
 begin
 
+  #
   # Recursive directory walk to find all files we want to check.  This is how
   # we handle deleted files, handle renames, etc. When a found file
   # is in the old checksum map we copy it over
+  #
 
   puts "Finding all files in #{directory}"
-  puts "\n"
+  start_time = Time.now
   Find.find(directory) do |path|
 
     if File.directory?(path)
@@ -72,6 +71,9 @@ begin
     end
   end
 
+  puts "\nFound all files in #{Time.now - start_time} seconds"
+  puts "\n"
+
   checksum_map = new_checksums # can toss out the checksum_map's memory
   files = checksum_map.keys
 
@@ -83,6 +85,7 @@ begin
   #
 
   start = Random.new.rand(files.count)
+  start_time = Time.now
 
   puts "Verifying #{files.count} files"
   [files[start..-1], files[0..start - 1]].each do |list|
@@ -99,9 +102,10 @@ begin
           puts "Verified #{short_path}"
         elsif modified_at == entry.file_modified_at
           puts "[ERROR] checksum changed: #{short_path}"
-          changed_files << { file_name: short_path, last_verified: entry.verified_at }
+          failed_files << OpenStruct.new(file_name: short_path, last_verified: entry.verified_at)
         else
           puts "[DEBUG] File changed: #{short_path}"
+          changed_files << OpenStruct.new(file_name: short_path, last_verified: entry.verified_at)
         end
       else
         puts "Added new file #{short_path}"
@@ -110,27 +114,36 @@ begin
 
     end
   end
+
+  puts "Verification complete in #{Time.now - start_time} seconds"
 rescue Interrupt
-  puts "Exiting early!"
+  puts "Exiting early -- saving progress!"
 rescue => e
   puts e.message
 end
 
+
 #
 # Write checksum file for next time
 #
+
 begin
   json = Oj.dump(checksum_map)
   File.open(checksum_file, 'w') { |f| f.write(json) }
-  puts "\nSuccessfully wrote checksums to #{checksum_file}"
+  puts "\nSuccessfully wrote #{files.count} checksums to #{checksum_file}"
   File.delete(backup_file)
 rescue => e
   puts e.message
 end
 
 if changed_files.any?
-  puts "\nAll changed files"
-  changed_files.each { |x| puts "#{x[:file_name]}, #{x[:last_verified]}" }
+  puts "\n[DEBUG] Changed files"
+  changed_files.each { |x| puts "#{x.file_name}" }
+end
+
+if failed_files.any?
+  puts "\nFound some possibly corrupted files"
+  failed_files.each { |x| puts "#{x.file_name}, #{x.last_verified}" }
 else
   puts "\nNo problems found!"
 end
